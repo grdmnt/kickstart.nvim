c64db672fdea48569c11795eb5bd5114d2f3456c
Gerard Montemayor <me@grdmnt.com>
2024-08-16 03:12:52 +1000

    add better multiselection for nvim telescope

--- init.lua
+++ init.lua
@@ -503,6 +503,22 @@ require('lazy').setup({
 
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
+      local actions = require 'telescope.actions'
+      local action_state = require 'telescope.actions.state'
+
+      local function fzf_multi_select(prompt_bufnr)
+        local picker = action_state.get_current_picker(prompt_bufnr)
+        local num_selections = #picker:get_multi_selection()
+
+        if num_selections > 1 then
+          -- actions.file_edit throws - context of picker seems to change
+          --actions.file_edit(prompt_bufnr)
+          actions.send_selected_to_qflist(prompt_bufnr)
+          actions.open_qflist()
+        else
+          actions.file_edit(prompt_bufnr)
+        end
+      end
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
@@ -513,6 +529,22 @@ require('lazy').setup({
         --   },
         -- },
         -- pickers = {}
+        defaults = {
+          mappings = {
+            i = {
+              -- close on escape
+              ['<esc>'] = actions.close,
+              ['<tab>'] = actions.toggle_selection + actions.move_selection_next,
+              ['<s-tab>'] = actions.toggle_selection + actions.move_selection_previous,
+              ['<cr>'] = fzf_multi_select,
+            },
+            n = {
+              ['<tab>'] = actions.toggle_selection + actions.move_selection_next,
+              ['<s-tab>'] = actions.toggle_selection + actions.move_selection_previous,
+              ['<cr>'] = fzf_multi_select,
+            },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
